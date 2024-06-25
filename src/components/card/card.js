3fdf7a43aec2e2f4aa533ee481d3c94f3827fcb3
Vishal Sharma <785vishal443@gmail.com>
2024-06-25 21:38:26 +0530

    state basic

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,8 +1,17 @@
 
-import Button from '../../button';
-import './card.css'
+import { useState } from 'react';
+import Button from '../../button';  // importing things from jsx or js file
+import './card.css' // importing the css file 
 
 const Card = () => {
+  // Play with JSX 
+  // hooks useState
+  let btnText = 'Click Me';
+  const [buttonText , setButtonText] = useState('Click Me123') // 
+  function handleClick() {
+    btnText = 'Updated'
+    setButtonText('Updated here..')
+  }
   return  (
     <div className="card">
       <div className="img-cont">
@@ -11,11 +20,54 @@ const Card = () => {
       <div>
         <h2>Heading</h2>
         <span>This is short description about the card</span>
+        <button onClick={handleClick}>{buttonText}</button>
+        {/* Create a button change description once click that chan ge the description of span tag */}
       </div>
       <Button/>
     </div>
   )
 }
-
-
 export default Card;
+
+
+
+// JSX
+// STATE 
+// PROPS 
+// useEffect => Next => React 
+// HOC
+// REACT DEPT nkjfg
+// Class Component
+// Advanced  => useCallback , useMemo , useRef 
+// Dom vs Virtual dom 
+// react pwrf
+
+// reconcilation 
+// render 
+// we will start building
+// context  , useReducer , useContext 
+
+
+
+
+
+
+// for reference
+
+// function useState(value){
+//   let data = value;
+//   console.log('initial',data)
+  
+//   function setData(updatedValue) {
+//     data =  updatedValue
+//       console.log(data)
+//   }
+//   return [data , setData]
+// }
+
+
+
+
+// const [a1 , setA1] = useState('Ramji');
+
+
