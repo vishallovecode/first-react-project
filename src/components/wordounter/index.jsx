7653366d303fc50f498782a7291989689fce138b
Vishal Sharma <785vishal443@gmail.com>
2024-07-08 22:54:29 +0530

    error message shown

--- src/components/wordounter/index.jsx
+++ src/components/wordounter/index.jsx
@@ -6,15 +6,18 @@ function WordCounter() {
   const [paragraph , setParagraph] = useState('')
   const[limit , setLimit] = useState(50)
   const [range , setRange]= useState('16')
+  const [errorMessage , setErrorMessage]= useState('')
 
   function onChangeLimit(e) {
     setLimit(e.target.value)
   }
   function onChange(event) {
       let count  = countWords(event.target.value);
+      console.log(event)
       if(count<=limit) {
         setParagraph(event.target.value)
       } else {
+        setErrorMessage(`Word  limit is ${limit} for text area , You have exhausted that limit. Please increase from above`)
         const s = event.target.value;
         const ss = s.split(' ').slice(0, limit).join(' ');
         setParagraph(ss)
@@ -22,6 +25,7 @@ function WordCounter() {
     
   }
   function countWords(value){
+    if(value ==='') return  0;
     return value?.split(' ').length;
   }
 
@@ -32,10 +36,11 @@ function WordCounter() {
     return (
       <div className='container'>
         <div>Font Size Picker</div>
-        <input type='range'value={range} min='16' max='32' onChange={rangeChange}/>
+        <input type='range'value={range} min={16} max={32} onChange={rangeChange}/>
         <div>Word Limit Count</div>
-        <input  type='number' defaultValue={limit}  onChange={onChangeLimit}/>
+        <input  type='number' defaultValue={limit}  onChange={onChangeLimit} />
         <textarea rows="10" cols="50" value={paragraph} onChange={onChange} style={{fontSize:`${range}px`}}/>
+        <div style={{color:'red'}}>{errorMessage}</div>
         <div>Total number of words written {countWords(paragraph)}</div>
         <div>Total number of character used {paragraph.length}</div>
       </div>
