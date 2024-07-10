717c83f57731ee980e9e3ad106a6cdecb9094426
Vishal Sharma <785vishal443@gmail.com>
2024-07-10 22:35:55 +0530

    Props example

--- src/components/propsExample/index.jsx
+++ src/components/propsExample/index.jsx
@@ -1,38 +1,89 @@
-function A(props){
+// function A(props){
+//   return  (
+//     <>
+//     <B description ={props.data} a={100} c= {50} d= {[1,2,3]} func = {()=>{console.log('Hey')}}/>
+//     <C heading ={props.data}/>
+//     </>
+//   )
+// }
+
+
+// function B(props){
+//     return  (
+//       <h3>
+//         {props.description}
+//       </h3>
+//     )
+// }
+
+// function C(props){
+// return  (
+//   <h3>
+//     {props.heading}
+//   </h3>
+// )
+// }
+
+
+// function PropsExample() {
+//   return (
+//     <div>
+//       <h2>Understanding Props ....</h2>
+//       <A data= {'Hello'}/>
+//     </div>
+//   )
+// }
+
+
+// export default PropsExample;
+
+
+function B(props) {
+  return (
+    <h2>{props.text}</h2>
+  )
+}
+
+function A(props) {
+  return  (
+    <C textC= {props.text2}/>
+  )
+}
+function C(props) {
+   // ??
+  return  (
+    <D heading={props.textC}/>
+  )
+}
+ function D(props){
   return  (
     <>
-    <B description ={props.data} a={100} c= {50} d= {[1,2,3]} func = {()=>{console.log('Hey')}}/>
-    <C heading ={props.data}/>
+      <h2>{props.heading}</h2>
+      <E subheading = {props.heading}/>
+    </>
+  )
+ }
+function E(props) {
+  // props.subheading ?? Props Learning
+  return  (
+    <>
+    <h2>{props.subheading}</h2>
+    <F mainheading= {props.subheading}/>
     </>
   )
 }
 
-
-function B(props){
-    return  (
-      <h3>
-        {props.description}
-      </h3>
-    )
-}
-
-function C(props){
-return  (
-  <h3>
-    {props.heading}
-  </h3>
-)
-}
-
-
-function PropsExample() {
+function F () {
   return (
-    <div>
-      <h2>Understanding Props ....</h2>
-      <A data= {'Hello'}/>
-    </div>
+    <h2>{props.mainheading}</h2>
   )
 }
 
-
-export default PropsExample;
+function P () {
+  return  (
+    <>
+      <B text = 'Props Learning'/>
+      <A text2= 'Props Learning'/>
+    </>
+  )
+}
