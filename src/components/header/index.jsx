046cf929aeff5921fe52976fc5406a4dedb81733
Vishal Sharma <785vishal443@gmail.com>
2024-08-08 22:42:11 +0530

    Routing intial

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,14 +1,19 @@
 import { NavLink } from 'react-router-dom';
 import './header.css'
+import { headerConstant } from '../../constant';
 const Header = () => {
   return(
     <div className='header'>
       <ul className='header-cont'>
-        <li className='item' >
-         <NavLink to='/square'> Square </NavLink>
-          </li>
-        <li className='item'>Card</li>
-        <li className='item'>Counter</li>
+          {
+            headerConstant.map((item)=>{
+              return  (
+                <li className='item' >
+                <NavLink to={item.link}> {item.name} </NavLink>
+                 </li>
+              )
+            })
+          }
       </ul>
     </div>
   )
--- src/constant.js
+++ src/constant.js
@@ -4,3 +4,24 @@ export const clothes = 'Clothes';
 export const utensils = 'Kitchen items..'
 export const table ="It is vishal table"
 export const myShoes = "This box have multiple shoes"
+
+
+export const headerConstant = [
+  {name: 'Square', link:'/square'},
+  {name: 'Card', link:'/card'},
+  {name: 'Counter', link:'/counter'},
+  {name: 'CustomCard', link:'/customCard'},
+  {name: 'JSXExample', link:'/jsxeample'},
+  {name: 'InlineStyling', link:'/inlinestyle'},
+  {name: 'Timer', link:'/timer'},
+  {name: 'ThemeChanger', link:'/theme'},
+  {name: 'WordCounter', link:'/wordcounter'},
+  {name: 'MovieCard', link:'/moviecard'},
+  {name: 'MovieListing', link:'/movie-listing'},
+  {name: 'PropsExample', link:'/props'},
+  {name: 'ExampleListing', link:'/examplelisting'},
+  {name: 'UseEffectExample', link:'/useffect'},
+  {name: 'ExampleKeys', link:'/key-example'},
+  {name: 'LifeCycleMethod', link:'/lifecycle'},
+  {name: 'ClassComponent', link:'/class-component'},
+]
