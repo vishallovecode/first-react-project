85873c13cd2e71276a1141a990add56a3fc124de
Vishal Sharma <785vishal443@gmail.com>
2024-08-27 21:45:48 +0530

    re-rendering

--- src/re-rendering/index.jsx
+++ src/re-rendering/index.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function ReRender (props) {
   let [count , setCount] = useState(0)
 
   function increment (){
     try {
-      setCount(count++ )
+      setCount(count++) //  0
+      setCount(++count) // 2
+      setCount(++count) // 3
+      setCount(count++) // 3
+      setCount(++count) // 5
+      setCount(++count) //  6
+      setCount(count++) // 6
+      console.log(count) //  7
     } catch(err){
       console.error('error::' , err)
     }
-
   }
+
+  useEffect(()=>{
+    setCount(count ++ ) // 7  // state === 7
+    // setCount(++ count )  // infinite rendering
+  } , [count])
 
   return (
     <h2>
