07ec10749e0a594a48cc14d79fc85945b4c10a77
Vishal Sharma <785vishal443@gmail.com>
2024-08-28 22:48:18 +0530

    memo  , useMemo , useCallback

--- src/optimization/index.jsx
+++ src/optimization/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react"
+import { useCallback, useMemo, useState } from "react"
 import Child  from  './Child'
 
 function Optimization () { 
@@ -15,12 +15,23 @@ function Optimization () {
     console.log('hey')
   }
 // useCallback memoized the whole function
-  const handleEmoizeFunc = useCallback(handleClick , [heading])
+  const handleEmoizeFunc = useCallback(handleClick , [])
+
+
+// memoizing the  value object or array or may be value return by the function
+  const res =  useMemo(()=>{
+    return {}
+  } , []) 
+
+
+  const re1 =  useMemo(()=>{
+    return [1,3,4,5,6,78,]
+  } , [heading]) 
 
   return (
     <div>
       <h1>{heading}</h1>
-      <Child handleClick ={handleEmoizeFunc}/>
+      <Child handleClick ={handleEmoizeFunc} res = {res}  re1 = {re1}/>
       <button onClick={handlHeading}>Change Heading</button>
     </div>
   )
